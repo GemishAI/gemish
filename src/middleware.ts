145313b7443e48bc569615aead8068df1e9719e7
Evansso <marsappollo3@gmail.com>
2025-03-12 10:37:48 +0300

    fixed redirection eerror in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,7 +16,7 @@ export async function middleware(request: NextRequest) {
   );
 
   if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/auth", request.url));
   }
 
   return NextResponse.next();
