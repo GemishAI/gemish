f6f1f392f9c815db50f1c5659c0019c652c0763a
Evansso <marsappollo3@gmail.com>
2025-03-12 10:40:22 +0300

    fixed middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,23 +4,7 @@ import { type NextRequest, NextResponse } from "next/server";
 
 type Session = typeof auth.$Infer.Session;
 
-export async function middleware(request: NextRequest) {
-  const { data: session } = await betterFetch<Session>(
-    "/api/auth/get-session",
-    {
-      baseURL: request.nextUrl.origin,
-      headers: {
-        cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
-      },
-    }
-  );
-
-  if (!session) {
-    return NextResponse.redirect(new URL("/auth", request.url));
-  }
-
-  return NextResponse.next();
-}
+export async function middleware(request: NextRequest) {}
 
 export const config = {
   matcher: ["/"],
